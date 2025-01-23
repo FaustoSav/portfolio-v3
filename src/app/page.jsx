7780e72267cb9cba058817d92cdb840b2fad27ee
Fausto Savoya <savoyafausto@gmail.com>
2025-01-22 23:45:18 -0300

    [REMOVE] redirect Timeout

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -1,27 +1,12 @@
-'use client';
-
-import { useEffect } from 'react';
-import { useRouter } from 'next/navigation';
-
+import { redirect } from 'next/navigation';
 export default function Home() {
-  const router = useRouter();
+  const preferredLocale = navigator.language.startsWith('es') ? 'es' : 'en';
+  redirect(`/${preferredLocale}`);
 
-  useEffect(() => {
-    const preferredLocale = navigator.language.startsWith('es') ? 'es' : 'en';
-
-    const timer = setTimeout(() => {
-      router.push(`/${preferredLocale}`);
-    }, 5000);
-
-    // Limpiar el timer al desmontar el componente
-    return () => clearTimeout(timer);
-  }, [router]);
 
   return (
-    <main className="grid place-items-center gap-2 w-screen h-screen">
-      <div className=""> Mira Bart, este es un loquito </div>
-      
-
+    <main className="grid place-items-center  w-screen h-screen ">
+      <div className=""> Mira Bart, este es un loquito</div>
     </main>
   );
 }
